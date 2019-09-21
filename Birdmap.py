0ac5f6e7bf31bbb9635705375a4cc5211ee9f557
Brady Nichols <school.bnichols@gmail.com>
2019-09-21 17:19:19 -0400

    Added 2nd layer of Tree Swallow data

--- Birdmap.py
+++ Birdmap.py
@@ -1,16 +1,18 @@
 import pandas as pd
 import folium
 
+statelatlongs = pd.read_csv("statelatlong.csv")
+
 url = 'https://raw.githubusercontent.com/python-visualization/folium/master/examples/data'
 state_geo = f'{url}/us-states.json'
-
-state_data = pd.read_csv("bbs2019.csv")
+state_data = pd.read_csv("easternbluebirds.csv")
+treeswallowdata = pd.read_csv("treeswallows.csv")
 
 m = folium.Map(location=[40, -98], zoom_start=4)
 
 folium.Choropleth(
     geo_data=state_geo,
-    name='choropleth',
+    name='Eastern Bluebirds',
     data=state_data,
     columns=['State', 'Success'],
     key_on='feature.id',
@@ -19,6 +21,31 @@ folium.Choropleth(
     line_opacity=0.2,
     legend_name='Success Rate (%)'
 ).add_to(m)
+
+folium.Choropleth(
+    geo_data=state_geo,
+    name='Tree Swallows',
+    data=treeswallowdata,
+    columns=['State', 'Success'],
+    key_on='feature.id',
+    fill_color='YlGnBu',
+    fill_opacity=0.7,
+    line_opacity=0.2,
+    legend_name='Success Rate (%)'
+).add_to(m)
+
+lats = statelatlongs["Latitude"]
+longs = statelatlongs["Longitude"]
+names = statelatlongs["State"]
+
+# fg = folium.FeatureGroup(name='Info')
+# for lat, lon, name in zip(lats, longs, names):
+#     html = f"""
+#     <h2>{name}<\h2><br>
+#     <img src=''>
+#     """
+#     fg.add_child(folium.CircleMarker(location=[lat, lon], popup=html, radius=4, fill=True, color='black'))
+# m.add_child(fg)
 
 folium.LayerControl().add_to(m)
 
