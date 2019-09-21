454b4c894b08ff4519b879f370d46d1ad0a75093
Brady Nichols <school.bnichols@gmail.com>
2019-09-21 16:16:36 -0400

    It works i think

--- Birdmap.py
+++ Birdmap.py
@@ -1,81 +1,23 @@
-#Note to reader: I'm sorry
-import os
-import pandas
+import pandas as pd
 import folium
-import numpy as np
-import json
-import requests
-
-#html for the popups
-html = """
-<head>
-<h4 style="margin-bottom:0px; padding-top:10px;">Volcano information:</h4>
-<p>
-Name: %s <br>
-Type: %s <br>
-successation: %s meters<br>
-</p>
-</head>
-
-<style>
-* {
-    font-family:Helvetica;
-    font-size:16;
-}
-
-</style>"""
 
 url = 'https://raw.githubusercontent.com/python-visualization/folium/master/examples/data'
-us_states = f'{url}/us-states.json'
+state_geo = f'{url}/us-states.json'
 
-geo_json_data = json.loads(requests.get(us_states).text)
+state_data = pd.read_csv("bbs2019.csv")
 
-data = pandas.read_csv("finalbbs2019.csv") #Opens the csv and sets it to the data variable
+m = folium.Map(location=[40, -98], zoom_start=4)
 
-state = list(data["Location"])
-success = list(data["Nesting Success Rate"]) #Sets each variable to a list of all the points in a given column
+folium.Choropleth(
+    geo_data=state_geo,
+    name='choropleth',
+    data=state_data,
+    columns=['State', 'Success'],
+    key_on='feature.id',
+    fill_color='YlGnBu',
+    fill_opacity=0.7,
+    line_opacity=0.2,
+    legend_name='Success Rate (%)'
+).add_to(m)
 
-datadict = {"State":state,
-            "Percent":success}
-
-def colorsuccess(success): #For later, returns a color based on the value of the successation
-    if success <20:
-        return "darkblue"
-    elif success <=40:
-        return "green"
-    elif success <=60:
-        return "blue"
-    elif success <=80:
-        return "purple"
-    elif success <=100:
-        return "orange"
-    else:
-        return "grey"
-
-BirdMap = folium.Map(location=[39.38, -118.63], zoom_start = 4) #Creates the basemap
-BirdMap.save("index.html") #Saves the map as an html file
-
-BirdMap.choropleth(
- geo_data=geo_json_data,
- name='choropleth',
- fill_color='red'
-)
-folium.LayerControl().add_to(BirdMap)
-
-
-'''
-fgVolc = folium.FeatureGroup(name="Volcanoes") #Creates a feature group for the volcanoes
-fgPop = folium.FeatureGroup(name="Population") #Creates a feature group for the population map
-
-fgPop.add_child(folium.GeoJson(data=open('world.json', 'r', encoding="utf-8-sig").read(), #(2 lines) Creates popualation map using a GeoJson (Don't ask how this works)
-style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
-
-for lat, lon, name, volc, success in zip(lat, lon, names, volc, success): #Creates the volcano map and adds it to the fgVolc feature group
-    iframe = folium.IFrame(html=html % (name, volc, success), width=300, height=122)
-    fgVolc.add_child(folium.CircleMarker(location=[lat, lon], popup=folium.Popup(iframe), radius=7, fill=True, color='grey', 
-    fill_opacity=0.7, fill_color=colorsuccess(success)))
-
-VolcanoMap.add_child(fgPop) #Adds population map to the final map
-VolcanoMap.add_child(fgVolc) #Adds volcanoes to the final map
-VolcanoMap.add_child(folium.LayerControl()) #Adds layer control, where you can toggle feature groups
-'''
+folium.LayerControl().add_to(m)
--- README.md
+++ README.md
@@ -1,9 +1,3 @@
-# Volcano-Map
+# Eastern-Bliebird-Map
 
 Here's the GitHub Pages link: https://bradyfish.github.io/BirdMapping/
-
-This is a map of all the volcanoes from the database here: http://volcano.oregonstate.edu/volcano_table
-
-I created this map using folium as part of this Python course on Udemy: https://www.udemy.com/the-python-mega-course/learn/v4/overview
-
-Thanks!
